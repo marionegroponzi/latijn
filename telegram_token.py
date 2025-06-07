a8d6994f88f4e2a7265569e594f585ab11bd4ba7
Mario Negro Ponzi <marionegroponzi@gmail.com>
2025-06-07 11:32:09 +0200

    Added telegram token based on keyring

---
+++ telegram_token.py
@@ -0,0 +1,16 @@
+import keyring
+
+def telegram_token():
+    """
+    Retrieves the Telegram token from the macOS keychain.
+    Ensure the token is stored in the keychain with the service name 'telegram' and key 'bot_token'.
+    """
+    return keyring.get_password("IlmariLatijnBot", "IlmariLatijnBot")
+
+def set_telegram_token(token):
+    """
+    Sets the Telegram token in the macOS keychain.
+    Use this function to store the token securely.
+    """
+    keyring.set_password("IlmariLatijnBot", "IlmariLatijnBot", token)
+    print("Telegram token set successfully.")
